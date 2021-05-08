7e8fbb0c3984cb6d5d7c2b06a828e0ccd8b6a091
B0YGABAS <isaiahandre.pabillon@g.msuiit.edu.ph>
2021-05-08 18:07:17 +0800

    W

--- W.py
+++ W.py
@@ -12,7 +12,7 @@ def hi():
     return "hello"
 
 '''
-@app.route('/')
+@app.route('/a')
 def a():
     ip_address = request.remote_addr
     print(ip_address)
@@ -26,7 +26,29 @@ def a():
     return ip_address + "----   "+ "".join(i for i in request.access_route)
     return "PAKYO!!!!!!!"
 
+@app.route('/m/<a>')
+def m(a):
+    if a=="pop":
+        session.pop("machine",None)
+        return "removed"
+    session["machine"]=a
+    return "machine added: "+a
+
+@app.route('/')
+@app.route('/p')
+def p():
+    return render_template('localstorage.html')
+    '''
+    if "machine" not in session:
+        return "no"
+    return session["machine"]'''
+    '''
+    print(request.headers.getlist("X-Forwarded-For"))
+    print(request.environ)
+    return "".join(i for i in request.environ)'''
+    #return "\n".join(i for i in request.headers.getlist("X-Forwarded-For"))
 
 if __name__ == "__main__":
+    #app.run()
     #app.run(host="192.168.18.241")
     app.run(host="0.0.0.0",port=8080,debug=True)
